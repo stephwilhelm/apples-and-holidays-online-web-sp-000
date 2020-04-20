67b27a77cd8672e193db0fbd76de711a84953301
Steph W <wilhelm.steph@gmail.com>
2020-04-20 22:55:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   # code here
   # remember to return the updated hash
   holiday_hash[:columbus_day] = {}
-	holiday_hash[:fall][:columbus_day] = "Flags", "Italian Food", "Parade Floats"
+	holiday_hash[:fall][:columbus_day] => "Flags", "Italian Food", "Parade Floats"
 	holiday_hash
 end
 
